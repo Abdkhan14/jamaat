85c2e7db46b12058bb13f0b272c69abc993e63f1
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2025-08-18 15:54:42 -0400

    Adding starter blocks of code

---
+++ backend/mosques.py
@@ -0,0 +1,30 @@
+MOSQUES = [
+    {
+        "name": "Baitul Aman",
+        "address": "3114 Danforth Ave, Scarborough, ON M1L 1B1",
+        "website": "https://danforthcommunitycenter.org/",
+        "latitude": 43.691767549675056,
+        "longitude": -79.28748266331257
+    },
+    {
+        "name": "Baitul Mukarram",
+        "address": "3340 Danforth Ave, Scarborough, ON M1L 1C6",
+        "website": "https://bmis.ca/",
+        "latitude": 43.694216937358604,
+        "longitude": -79.27774140584236
+    },
+    {
+        "name": "Masjid Al-Abedeen",
+        "address": "1 Stamford Square N, Scarborough, ON M1L 1X4",
+        "website": "https://irccan.com/",
+        "latitude": 43.71148514428229,
+        "longitude": -79.29503332832792
+    },
+    {
+        "name": "The Sunatul Jamaat",
+        "address": "347 Danforth Rd, Scarborough, ON M1L 3X8",
+        "website": "https://danforthcommunitycenter.org/",
+        "latitude": 43.70370861787685,
+        "longitude": -79.26959493102808
+    }
+]
